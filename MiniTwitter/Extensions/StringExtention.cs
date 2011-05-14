0185b1847996adfec4707f41c0fae46079e972f3
iyomumx <mo.yi.sam@gmail.com>
2011-05-15 01:24:40 +0800

    License Declare
    Signed-off-by: iyomumx <mo.yi.sam@gmail.com>

--- MiniTwitter/Extensions/StringExtention.cs
+++ MiniTwitter/Extensions/StringExtention.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * 此文件由Apache License 2.0授权的文件修改而来
+ * 根据协议要求在此声明此文件已被修改
+ * 
+ * 未被修改的原始文件可以在
+ * https://github.com/iyomumx/MiniTwitter-Mod/tree/minitwitter
+ * 找到
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
